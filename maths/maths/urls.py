ab327f471cb2aced323b85f19ae2a976b2fe844a
jyesvanthe <jyesvanthev@gmail.com>
2024-05-07 11:48:00 +0530

    Success

---
+++ maths/maths/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from max import views
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('surfaceareaofcylinder/',views.surfacearea,name="surfaceareaofcylinder"),
+    path('',views.surfacearea,name="surfaceareaofcylinderroot")
+]
